c7761eaf652d64fe4b6442c2ffb124ad370ffa68
01971164 <CA01971164@st.kawahara.ac.jp>
2025-06-19 19:51:04 +0900

    style:CSSを追加
    refactor:ソースコードの関数化

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,16 +1,61 @@
-const hour = document.getElementById("hour") as HTMLSpanElement;
-const min = document.getElementById("min") as HTMLSpanElement;
-const second = document.getElementById("second") as HTMLSpanElement;
-const btn = document.getElementById("btn") as HTMLButtonElement;
+function setUp() {
+  // ID省略関数
+  function $(id: string) {
+    const id1 = document.getElementById(id);
+    return id1;
+  }
 
-btn.addEventListener("click", () => {
-  updateClock();
-  setInterval(updateClock, 1000);
-});
+  //マネージドクラス
+  class manageTime {
+    constructor() {
+      this.time = 0;
+    }
+    private time: number;
 
-function updateClock() {
-  const date = new Date();
-  hour.textContent = String(date.getHours());
-  min.textContent = String(date.getMinutes());
-  second.textContent = String(date.getSeconds());
+    setTime() {
+      updateClock();
+      this.time = setInterval(updateClock, 1000);
+    }
+
+    clearTime() {
+      clearInterval(this.time);
+    }
+  }
+
+  //アップデート関数
+  function updateClock() {
+    if (hour && min && second) {
+      const date = new Date();
+      hour.textContent = String(date.getHours());
+      min.textContent = String(date.getMinutes());
+      second.textContent = String(date.getSeconds());
+    }
+  }
+
+  const time = new manageTime();
+
+  const min = $("min");
+  const hour = $("hour");
+  const second = $("second");
+  const btn = $("btn");
+
+  //ボタンクリック関数
+  btn?.addEventListener("click", () => {
+    if (min && hour && second) {
+      min.textContent = "";
+      hour.textContent = "";
+      second.textContent = "";
+      console.log(min);
+    }
+
+    time.clearTime();
+    time.setTime();
+  });
 }
+
+function main() {
+  setUp();
+}
+
+// メインクラス
+main();
